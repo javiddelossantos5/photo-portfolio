d1be028b2fc8870b310865a0e7e06b1074987b8f
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-05 11:53:13 -0600

    form accepts date iput by UI and handles submission click of the button

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    // Hook that will manage the form data
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    // destructure the formState into its named properties name, email message
+    const { name, email, message } = formState;
+
+    //handles input put by the user into form
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    };
+    
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name'/>
+                    <input type='text' defaultValue={name} onChange={handleChange} name='name' />
                 </div>
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email'/>
+                    <input type='email' defaultValue={email} name='email' onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <input type='message' rows='5'/>
+                    <textarea name='message' defaultValue={message} rows='5' onChange={handleChange} />
                 </div>
                     <button type='submit'>Submit</button>
             </form>
         </section>
-    );
+    )
 };
 
-export default ContactFrom;
+export default ContactForm;
